6f1778faeefac0e3043772586fe79b4220c35a55
Yasuhisa Arakawa <hal.academy.student.a@gmail.com>
2013-11-26 00:41:01 +0900

    Initial Commit

---
+++ iBeaconDetectore/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iBeaconDetectore
+//
+//  Created by yasuhisa.arakawa on 2013/11/26.
+//  Copyright Yasuhisa Arakawa 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
